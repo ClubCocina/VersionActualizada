436781e1f4dc525a14c3087f32d1c25ddf62195b
guaso <pablotomasmartinez@gmail.com>
2015-09-23 22:32:06 -0300

    cambios CSS

--- application/views/detallechef/ver_chef.php
+++ application/views/detallechef/ver_chef.php
@@ -38,7 +38,7 @@ https://twitter.com/intent/tweet?text=<?= urlencode(base_url($chefLink))?>"><i c
                 <?php endforeach; ?>
             </div>
             <div id="metas">
-                <div class="mayus titulo-tags white">ESPECIALIDADES</div>
+                <div class="mayus titulo-tags white">Especialidades</div>
                 <div><?php if (!empty($tagsChef)): ?>
                     <ul>
                             <?php foreach ($tagsChef as $tagChef): ?>
@@ -93,17 +93,17 @@ https://twitter.com/intent/tweet?text=<?= urlencode(base_url($chefLink))?>"><i c
             </div>
         </div>
         <div id="invitados-comunas" class="bg-rojo white overflowauto">
-            <div>REQUISITOS DEL CHEF</div>
+        <!--    <div>REQUISITOS DEL CHEF</div>  -->
             <div id="maxpersonas">
                 <span class="preview-clp mayus">RANGO DE COMENSALES</span>
                 <br>
                 <div class="info">
                     <img src="<?= base_url('images/max-invitados.png'); ?>"/>
-                    <span class="mayus">De <?= str_replace('-', ' a ', $parametrosChef['5']) ?> Personas</span>
+                    <span>De <?= str_replace('-', ' a ', $parametrosChef['5']) ?> Personas</span>
                 </div>
             </div>
             <div id="comunas">
-                <span class="preview-clp mayus">COMUNAS A LAS QUE LLEGA</span>
+                <span class="preview-clp mayus">DONDE COCINA</span>
                 <div class="info"><img src="<?= base_url('images/comunas.png'); ?>" class="float-left"/>
                     <?php if (isset($comunas)): ?>
                         <ul class="float-left">
