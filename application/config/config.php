24402e05f07e15a448eba16ee844ec933645977e
janosoto <alejandro.soto@usach.cl>
2015-10-03 19:12:58 -0300

    Restauración de config.php con ruta localhost:8888

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/VersionActualizada';
+$config['base_url']	= 'http://localhost:8888/VersionActualizada';
 /*
 |--------------------------------------------------------------------------
 | Index File
