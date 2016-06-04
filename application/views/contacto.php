bf57d9f9455d77b88c92afdbad0bfbcc4da29575
guaso <pablotomasmartinez@gmail.com>
2016-06-04 17:09:01 -0400

    texto contacto + css

--- application/views/contacto.php
+++ application/views/contacto.php
@@ -1,18 +1,18 @@
 <div class="row">
     <div class="col-xs-10 col-xs-offset-1">
         <div id="contacto" class="bg-color-general">
-            <div class="titulo-contacto mayus">CONTÁCTANOS</div>
+            <div class="titulo-contacto">Llámanos al <a href="tel:+56957832786">+56 9 5783 2786</a> o déjanos un mensaje</div>
             <?= form_open('home/contacto') ?>
             <div class="col-sm-6 col-xs-12">
                 <div class="contacto-fields">
-                    <div class="label">Nombre*</div>
+                    <div class="label">Nombre *</div>
                     <div>
                         <input type="text" name="nombre"/>
                         <div class="error-validacion"><?= form_error('nombre') ?></div>
                     </div>
                 </div>
                 <div class="contacto-fields">
-                    <div class="label">Correo*</div>
+                    <div class="label">Correo *</div>
                     <div>
                         <input type="text" name="mail"/>
                         <div class="error-validacion"><?= form_error('mail') ?></div>
@@ -27,7 +27,7 @@
                 </div>
             </div>
             <div class="col-sm-6 col-xs-12">
-                <div>Mensaje*</div>
+                <div>Mensaje *</div>
                 <textarea name="mensaje"></textarea>
                 <div class="error-validacion"><?= form_error('mensaje') ?></div>
             </div>
